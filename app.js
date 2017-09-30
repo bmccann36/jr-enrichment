fdc18443552d141f1d95ee19e723d88a4a347a85
Brian McCann <bmccann36@gmail.com>
2017-09-30 15:33:52 -0400

    db portion working

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express');
+ const express = require('express');
 const app = express();
 
 const db = require('./db').db;
 const Student = require('./db').Student;
 const Teacher = require('./db').Teacher;
+const chalk = require('chalk')
 
 let PORT = 8080;
 
@@ -11,12 +12,33 @@ app.get("/test", (req, res, next) => {
 	// Visit http://localhost:8080/test to see the message!
 	res.send("Hello GET Route!")
 })
-/* 
+/*
  Your Route Code Here
 */
 
-db.sync({force: true})
+db.sync({
+	// force: true
+})
 .then(() => {
 	console.log('db synced')
 	app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
-});
+})
+.then(()=>{
+		Student.findPerfect()
+		.then(perfects =>{
+			green(perfects[0].name)
+		})
+})
+		// 	.then(() => {
+		// 	Student.create({
+		// 		name: 'Robert',
+		// 		gpa: 4
+		// 	})
+		// })
+
+function green(string){
+	console.log(chalk.green(string))
+}
+
+
+
--- db.js
+++ db.js
@@ -6,16 +6,49 @@ const db = new Sequelize('postgres://localhost/juniorenrichment', {
 });
 
 
-const Student = db.define("student" , {
-	/* STUDENT MODEL CODE HERE */
-
+const Student = db.define('student', {
+	name: Sequelize.STRING,
+	gpa: Sequelize.INTEGER
 })
-	
+
+Student.prototype.getLetter = function(){
+	if (this.gpa >= 3) return 'A'
+	else if (this.gpa >= 2) return 'B'
+	else if (this.gpa >= 1) return 'C '
+}
+
+Student.findPerfect = function(){
+	return this.findAll({
+		where: {gpa: 4}
+	})
+}
+
+// Page.findByTag = function (tag) {
+// 	return this.findAll({
+// 			where: {
+// 					tags: {
+// 							$contains: [tag]
+// 					}
+// 			}
+// 	});
+
 
 const Teacher = db.define('teacher', {
-	/* TEACHER MODEL CODE HERE */
-
+	name: Sequelize.STRING,
+	subject: Sequelize.STRING
 });
+
+Student.belongsTo(Teacher)
 
 
 module.exports = {db, Student, Teacher}
+
+
+// Student.prototype.getLetter = function () {
+// 	return
+// 	let letGrade;
+// 	let intGrade = this.gpa
+// 	if (intGrade >= 3) letGrade = 'A'
+// 	else if (intGrade >= 2) letGrade = 'B'
+// 	return letGrade
+// }
